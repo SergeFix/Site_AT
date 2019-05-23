6ad7bbdb186f796a6c21850785ce451131e638b1
SergeFix <serge_fx@tut.by>
2019-05-24 00:10:15 +0300

    deleted:  landing_page.py file

--- landing_page.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
-
-class LandingPage():
-    """A class for landing page locators and methods."""
-    def __init__(self, driver):
-        self.driver = driver
-
-        self.
